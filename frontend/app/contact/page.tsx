cdc4425a59538f6ba67d8e3427e57c79e8e44455
y-shiala <yvanashivon@gmail.com>
2025-05-27 22:32:23 +0300

    Added new pages

---
+++ frontend/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      <h1>Landing</h1>
+    </div>
+  )
+}
+
+export default contact;
---
+++ frontend/app/landing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const landing = () => {
+  return (
+    <div>
+      <h1>Landing</h1>
+    </div>
+  )
+}
+
+export default landing;
