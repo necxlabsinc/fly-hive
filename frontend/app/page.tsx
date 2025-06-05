86fd7251c04182549c06e66dd7a9d8fb037e3757
y-shiala <yvanashivon@gmail.com>
2025-06-05 14:10:32 +0300

    first-commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
-import Hero from "./landing/Hero";
+import Hero from "../components/landing/Hero";
+import { div } from "framer-motion/client";
+import Landing from "../components/landing/Card-landing"
+import Providers from "../components/landing/providers"
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Landing />
+      <Providers />
     </>
   );
 }
---
+++ frontend/components/landing/card-data.tsx
@@ -0,0 +1,44 @@
+export default [
+  {
+    Img: "/plane-7.jpg",
+    title: "flight 901",
+    departure: "9:00am",
+    arrival: "10:15am",
+    price: "ksh 13,000"
+  },
+  {
+    Img: "/plane-2.jpg",
+    title: "flight 901",
+    departure: "9:00am",
+    arrival: "10:15am",
+    price: "ksh 13,000"
+  },
+  {
+    Img: "/plane-8.jpg",
+    title: "flight 901",
+    departure: "9:00am",
+    arrival: "10:15am",
+    price: "ksh 13,000"
+  },
+  {
+    Img: "/plane-1.jpg",
+    title: "flight 901",
+    departure: "9:00am",
+    arrival: "10:15am",
+    price: "ksh 13,000"
+  },
+/*   {
+    Img: "/plane-1.jpg",
+    title: "flight 901",
+    departure: "9:00am",
+    arrival: "10:15am",
+    price: "ksh 13,000"
+  }, */
+ /*  {
+    Img: "/plane-1.jpg",
+    title: "flight901",
+    departure: "9:00am",
+    arrival: "10:15am",
+    price: "ksh 13,000"
+  } */
+]
